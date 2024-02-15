dc47052481716038d6624ffac14e229717fd5d0c
Jabriel <jj.schafer@hotmail.com>
2024-02-15 11:28:54 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/defaults";
+import { GlobalStyle } from "./styles/global";
+import { Transactions } from "./pages/Transactions";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
+
+export function App() {
+
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <TransactionsProvider>
+       <Transactions />
+
+      </TransactionsProvider>
+
+    </ThemeProvider>
+  )
+}
